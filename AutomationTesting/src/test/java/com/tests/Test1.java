b9fb2707a44b463ab4571dae4d3c41552d200863
Chetan <Chetan@DESKTOP-LL5DE2O>
2021-04-17 19:48:30 +0530

    these is my first commite

---
+++ AutomationTesting/src/test/java/com/tests/Test1.java
@@ -0,0 +1,19 @@
+package com.tests;
+
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.testng.annotations.Test;
+
+public class Test1 {
+
+	@Test
+	public void openChromeBrowser()
+	{
+		System.setProperty("webdriver.chrome.driver", "E:\\selenium programs\\AutomationTesting\\Drivers\\chromedriver.exe");
+		WebDriver driver = new ChromeDriver();
+		driver.manage().window().maximize();
+		driver.get("https://www.facebook.com/");
+	}
+	
+	
+}
---
+++ AutomationTesting/src/test/java/com/tests/package-info.java
@@ -0,0 +1,8 @@
+/**
+ * 
+ */
+/**
+ * @author Chetan
+ *
+ */
+package com.tests;
